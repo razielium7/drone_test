3e60a247be77a8906097a70e8bd4f7da5e9c8a13
Vlad <vladimir.kolpakov@agatatechnology.es>
2021-06-02 13:20:14 +0200

    changed port to 3306

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 
 var app = express();
-const port = 3001;
+const port = 3306;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
